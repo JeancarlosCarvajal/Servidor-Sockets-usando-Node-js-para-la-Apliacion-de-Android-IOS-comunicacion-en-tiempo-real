1d3675f15c1bc3c3fbc054fc2e170fdad6561886
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-07-22 16:50:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+
+// Path publico
+
+const publicPath = path.resolve(__dirname, 'public');
+
+
+app.use(express.static(publicPath));
+
+
+
+app.listen(3000, ( err ) => {
+
+    if(err) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto !!!!!', 3000);
+
+});
