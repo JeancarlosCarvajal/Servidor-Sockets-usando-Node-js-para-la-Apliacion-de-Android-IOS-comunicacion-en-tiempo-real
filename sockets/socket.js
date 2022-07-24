f5fc56a7c503a9f9d55f2b837e522d510e463284
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-07-24 15:28:33 -0400

    Servidor listo para produccion y demostracion, comunicacion en tiempo real usadno Sockets de Node.js y modulo express

--- sockets/socket.js
+++ sockets/socket.js
@@ -7,11 +7,12 @@ const Band = require('../models/band');
 // llamamos las clases de bandas
 const bands = new Bands();
 
-// anadir una nuewva banda
+// anadir una nuewva banda. aqui se crean
 bands.addBand( new Band('Queen') );
 bands.addBand( new Band('Bon Jovi') );
 bands.addBand( new Band('Heroes del Silecio') );
 bands.addBand( new Band('Metalica') ); 
+bands.addBand( new Band('Mana') ); 
 
 // console.log(bands);
 
@@ -27,13 +28,37 @@ io.on('connection', client => {
       console.log('Mensaje !!!', payload); 
       io.emit('mensaje', {admin:'Nuevo mensaje II'}); 
     });
-  
-    // escuchar el emitir-mensaje desde el cliente
-    client.on('emitir-mensaje', payload => { 
-      console.log('Emitir Mensaje !!!', payload);  
-      io.emit('emitir-mensaje', payload); // lo emite a todos
-      // client.broadcast.emit('emitir-mensaje', payload); // lo emite a todos menos el que lo emitio
+
+    // escuchar el vote-band desde el cliente
+    client.on('vote-band', payload => { 
+      console.log('Vote por:', payload);  
+      bands.voteBand(payload.id);
+      io.emit('active-bands', bands.getBands()); // lo emite a todos
+      // client.broadcast.emit('active-bands', payload); // lo emite a todos menos el que lo emitio
     });
+
+    // escuchar el add-band desde el cliente
+    client.on('add-band', payload => { 
+      console.log('add por:', payload);  
+      bands.addBand(new Band(payload.name));
+      io.emit('active-bands', bands.getBands()); // lo emite a todos
+      // client.broadcast.emit('active-bands', payload); // lo emite a todos menos el que lo emitio
+    });
+
+    // escuchar el delete-band desde el cliente
+    client.on('delete-band', payload => { 
+      console.log('delete a:', payload);  
+      bands.deleteBand(payload.id);
+      io.emit('active-bands', bands.getBands()); // lo emite a todos
+      // client.broadcast.emit('active-bands', payload); // lo emite a todos menos el que lo emitio
+    });
+  
+    // // escuchar el emitir-mensaje desde el cliente
+    // client.on('emitir-mensaje', payload => { 
+    //   console.log('Emitir Mensaje !!!', payload);  
+    //   io.emit('emitir-mensaje', payload); // lo emite a todos
+    //   // client.broadcast.emit('emitir-mensaje', payload); // lo emite a todos menos el que lo emitio
+    // });
   
   
   
